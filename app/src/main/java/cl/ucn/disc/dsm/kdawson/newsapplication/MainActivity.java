311fb3e7f0afa0b967a51f91116cc4d24c60fc4e
KevinDD97 <kevin.dawson@alumnos.ucn.cl>
2021-12-23 02:18:30 -0400

    Updates

--- app/src/main/java/cl/ucn/disc/dsm/kdawson/newsapplication/MainActivity.java
+++ app/src/main/java/cl/ucn/disc/dsm/kdawson/newsapplication/MainActivity.java
@@ -1,12 +1,17 @@
 package cl.ucn.disc.dsm.kdawson.newsapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     //b7e306faac694854bb1521295127bfa7
+
+    private RecyclerView newsRV, categoryRV;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/cl/ucn/disc/dsm/kdawson/newsapplication/NewsDeatilActivity.java
+++ app/src/main/java/cl/ucn/disc/dsm/kdawson/newsapplication/NewsDeatilActivity.java
@@ -11,5 +11,11 @@ public class NewsDeatilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_deatil);
+        title=getIntent().getStringExtra("title");
+        desc=getIntent().getStringExtra("desc");
+        content=getIntent().getStringExtra("content");
+        imageURL=getIntent().getStringExtra("image");
+        url=getIntent().getStringExtra("url");
+
     }
 }
